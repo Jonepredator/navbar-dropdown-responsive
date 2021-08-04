c03268f75c93aa0bd5bd737eefe3cb0c1737af6f
Juan Fuenzalida <70103443+Jonepredator@users.noreply.github.com>
2021-08-04 15:13:23 +0200

    Dropdown.js ,dropdown.css, added menu items and navbar.css

---
+++ src/components/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+   {
+      title: 'Design',
+      path: '/design',
+      cName: 'dropdown-link'
+   },
+   {
+      title: 'Development',
+      path: '/development',
+      cName: 'dropdown-link'
+   },
+   {
+      title: 'Extra1',
+      path: '/extra1',
+      cName: 'dropdown-link'
+   },
+   {
+      title: 'Extra2',
+      path: '/extra2',
+      cName: 'dropdown-link'
+   },
+]
